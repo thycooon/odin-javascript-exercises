ea9986a49f0226533ca5fdb4290256259010ed67
thycooon <117923868+thycooon@users.noreply.github.com>
2023-11-01 15:09:10 +0100

    Solved: 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...elementsToRemove) {
+    if (Array.isArray(array)){
+        if (elementsToRemove.length > 0){
+            elementsToRemove.forEach(element => {
+                if (array.includes(element)){
+                    let elementIdx = array.indexOf(element)
+                    array = [...array.slice(0,elementIdx),...array.slice(elementIdx+1)]
+                }
+            })
+            return array;
+        } else {
+            return array;
+        }
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
