07ffb277e9ba0943c6d1d4d231f535e94ee938bf
thycooon <117923868+thycooon@users.noreply.github.com>
2023-11-01 15:30:29 +0100

    Solved: 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(intNum1, intNum2) {
+    if (typeof intNum1 === "number" && typeof intNum2 ==="number"){
+        if (intNum1 >= 0 && intNum2 >= 0){
+            let sum = 0;
+            let i = (intNum1 < intNum2) ? intNum1 : intNum2;
+            let limit = (intNum1 < intNum2) ? intNum2 : intNum1;
+            for (i; i<=limit; i++){
+                sum += i;
+            }
+            return sum
+        }
+    }
+    return "ERROR"
 };
 
 // Do not edit below this line
