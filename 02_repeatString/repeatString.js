c4808ad895f1f1b31c3b1394885d3f24cc70d944
thycooon <117923868+thycooon@users.noreply.github.com>
2023-11-01 12:14:57 +0100

    Solved: 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (typeof string === "string" && typeof num === "number"){
+        if (num === 0 || string === '') {
+            return ''
+        } else if (num > 0 && string.length > 0) {
+            let newString = "";
+            for (let i=0; i<num; i++){
+                newString += string;
+            }
+            return newString
+        }
+    }
 
+    return "ERROR"
 };
 
 // Do not edit below this line
